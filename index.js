406f801979d20dbadd27df6c44cb4e5751a6fdc4
srihanth-m <srishanthmule@gmail.com>
2023-09-15 13:42:38 +0530

    built the tool here

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const replaceThis = "harry"
+const replaceWith = "srishanth"
+const preview = true;
+
+try {
+  fs.readdir("data" , (err , data) =>{
+    for(let index = 0 ; index < data.length ; index++){
+        const item = data[index];
+        let newFile = "data/" + item.replaceAll(replaceThis , replaceWith)
+        if(!preview){
+            fs.rename("data/" + item , newFile , () =>{
+                console.log("Rename Success");
+            })
+        }else{
+            if(!"data/" +item != newFile)console.log("data/" + item +" will be renamed to " + newFile)
+        }
+    }
+})
+} catch (err) {
+  console.error(err);
+}
